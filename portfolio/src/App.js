7452f3a707e64420b58bdfc760e2fb0e4cab20f3
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-04-19 18:22:26 +1000

    add in other routes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,17 +4,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Header from "./components/Header/Header";
+import Projects from "./components/Projects/Projects";
+import Resume from "./components/Resume/Resume";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
     <div>
-      {/* <Home /> */}
-      {/* <About /> */}
       {/* <Header /> */}
       <Router> 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
        </Router>
     </div>
