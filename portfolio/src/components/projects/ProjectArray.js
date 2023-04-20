ff5f7be699fee871b560d83fda0b5d2f764111f4
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-04-20 20:26:02 +1000

    update project files

--- portfolio/src/components/projects/ProjectArray.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Projects() {
-    return (
-        <div className="main-container">
-            <h1> Project page </h1>
-        </div>
-      )
-  }
