1ab2f88550140d490eefa3850693d22c72dccbb1
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-04-21 09:15:11 +1000

    add basic contact form

--- portfolio/src/components/Contact/Contact.js
+++ portfolio/src/components/Contact/Contact.js
@@ -1,9 +1,32 @@
 import React from "react";
+import "./Contact.css";
 
 export default function Contact() {
     return (
         <div>
-            <h1> Contact page </h1>
+            <form className="form">
+                <label>Name:</label>
+                <input
+                    value="name"
+                    name="name"
+                    type="text"
+                    placeholder="Name"
+                />
+                <label>Email address:</label>
+                <input
+                    value="email"
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                />
+                <label>Message:</label><br></br>
+                <textarea rows="15" cols="50"
+                    value="message"
+                    name="message"
+                    type="message"
+                /><br></br>
+                <button type="button" id="submitBtn">Submit</button>
+            </form>
         </div>
-      )
+    );
   }
