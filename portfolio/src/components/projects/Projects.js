fd4802a8780da21ccea7c171b985552b729a240a
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-04-20 20:21:50 +1000

    restructure projects folder

--- portfolio/src/components/projects/Projects.js
+++ portfolio/src/components/projects/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Projects() {
     return (
-        <div>
+        <div className="main-container">
             <h1> Project page </h1>
         </div>
       )
