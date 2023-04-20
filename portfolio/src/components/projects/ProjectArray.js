8a841309aa572411e27e30b583d289269e18dc91
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-04-20 20:58:58 +1000

    set up initial projects array

---
+++ portfolio/src/components/projects/ProjectArray.js
@@ -0,0 +1,33 @@
+import blank from "../../assets/blank.pdf";
+import quizDemo from "../../assets/quiz-demo.gif";
+
+export const projectArray = [
+    {
+        id: 1,
+        name: "Share Shed",
+        deployedLink: "https://obscure-woodland-19082.herokuapp.com/",
+        repoLink: "https://github.com/wolldog/share-shed",
+        gif: blank,
+      },
+      {
+        id: 2,
+        name: "Weather Dashboard",
+        deployedLink: "https://victoriacook.github.io/weather-forecast/",
+        repoLink: "https://github.com/VictoriaCook/weather-forecast",
+        gif: blank,
+      },
+      {
+        id: 3,
+        name: "JavaScript Quiz",
+        deployedLink: "https://victoriacook.github.io/javascript-quiz",
+        repoLink: "https://github.com/VictoriaCook/javascript-quiz",
+        gif: quizDemo,
+      },
+      {
+        id: 4,
+        name: "Ecommerce Backend",
+        deployedLink: "https://watch.screencastify.com/v/KiuAM3UqTiX7nWSWexnJ",
+        repoLink: "https://github.com/VictoriaCook/ecommerce-backend",
+        gif: blank,
+      }
+];
