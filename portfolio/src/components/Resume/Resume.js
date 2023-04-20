bdc5c9bfe78712dccf0738b8ca928bbdc1efc96b
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-04-20 20:14:11 +1000

    add basic resume component and download btn

--- portfolio/src/components/Resume/Resume.js
+++ portfolio/src/components/Resume/Resume.js
@@ -1,9 +1,22 @@
 import React from "react";
+import blank from '../../assets/blank.pdf';
+import './Resume.css';
 
 export default function Resume() {
-    return (
-        <div>
-            <h1> Resume page </h1>
-        </div>
-      )
+       const handleDownload = () => {
+          const link = document.createElement('a');
+          link.href = blank;
+          link.download = '../assets/blank.pdf';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+      };
+      return (
+          <div className="main-container">
+              <p>Download my resume below</p>
+              <button onClick={handleDownload}>
+                  Download
+              </button>
+          </div>
+      );
   }
