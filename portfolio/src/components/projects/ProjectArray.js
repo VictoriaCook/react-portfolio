6f5d3fab9efc003046e9c2a96e9dafc298cb1f75
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-04-20 20:25:54 +1000

    restructure project setup

---
+++ portfolio/src/components/projects/ProjectArray.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Projects() {
+    return (
+        <div className="main-container">
+            <h1> Project page </h1>
+        </div>
+      )
+  }
