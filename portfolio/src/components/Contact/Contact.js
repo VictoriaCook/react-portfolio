b9b3127ed316c6a03f6cbe88ca398aedd6a54f6b
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-04-21 09:33:07 +1000

    set up basic css for contact form

--- portfolio/src/components/Contact/Contact.js
+++ portfolio/src/components/Contact/Contact.js
@@ -3,28 +3,33 @@ import "./Contact.css";
 
 export default function Contact() {
     return (
-        <div>
+        <div className="main-container">
             <form className="form">
                 <label>Name:</label>
+                <br></br>
                 <input
                     value="name"
                     name="name"
                     type="text"
                     placeholder="Name"
                 />
+                <br></br>
                 <label>Email address:</label>
+                <br></br>
                 <input
                     value="email"
                     name="email"
                     type="email"
                     placeholder="Email"
                 />
+                <br></br>
                 <label>Message:</label><br></br>
                 <textarea rows="15" cols="50"
                     value="message"
                     name="message"
                     type="message"
-                /><br></br>
+                />
+                <br></br>
                 <button type="button" id="submitBtn">Submit</button>
             </form>
         </div>
