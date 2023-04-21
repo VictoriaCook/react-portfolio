9155ac679cee5c8a94cd3e10cff0596b729e4ea4
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-04-21 11:25:22 +1000

    remove placeholders in form

--- portfolio/src/components/Contact/Contact.js
+++ portfolio/src/components/Contact/Contact.js
@@ -49,7 +49,7 @@ export default function Contact() {
                     value={name}
                     name="name"
                     type="text"
-                    placeholder="Name"
+                    // placeholder="Name"
                     onChange={handleInputChange}
                 />
                 <br></br>
@@ -59,7 +59,7 @@ export default function Contact() {
                     value={email}
                     name="email"
                     type="email"
-                    placeholder="Email"
+                    // placeholder="Email"
                     onChange={handleInputChange}
                 />
                 <br></br>
@@ -68,6 +68,7 @@ export default function Contact() {
                     value={message}
                     name="message"
                     type="message"
+                    // placeholder="Message"
                     onChange={handleInputChange}
                 />
                 <br></br>
