c7454379d5ead466e2f75956d6534ef98df5d695
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-04-21 21:24:41 +1000

    add remaining gifs to projects page

--- portfolio/src/components/projects/ProjectArray.js
+++ portfolio/src/components/projects/ProjectArray.js
@@ -1,6 +1,9 @@
 import shareShed from "../../assets/shareShed.gif";
 import weatherApp from "../../assets/weatherApp.gif";
 import quizDemo from "../../assets/quizDemo.gif";
+import ecommerceBackend from "../../assets/ecommerceBackend.gif";
+import socialNetworkApi from "../../assets/socialNetworkApi.gif";
+import teamProfileGenerator from "../../assets/teamProfileGenerator.gif"
 
 const ProjectArray = [
     {
@@ -29,21 +32,21 @@ const ProjectArray = [
         name: "Ecommerce Backend",
         deployedLink: "https://watch.screencastify.com/v/KiuAM3UqTiX7nWSWexnJ",
         repoLink: "https://github.com/VictoriaCook/ecommerce-backend",
-        gif: quizDemo,
+        gif: ecommerceBackend,
       },
       {
         id: 5,
-        name: "Social Network API",
-        deployedLink: "https://watch.screencastify.com/v/tZbTfOlcRNlVmJXwpcUy",
-        repoLink: "https://github.com/VictoriaCook/social-network-api",
-        gif: quizDemo,
-      },
-      {
-        id: 6,
         name: "Team Profile Generator",
         deployedLink: "https://watch.screencastify.com/v/1ImIHltgNpddxWWugrTX",
         repoLink: "https://github.com/VictoriaCook/team-profile-generator",
-        gif: quizDemo,
+        gif: teamProfileGenerator,
+      },
+      {
+        id: 6,
+        name: "Social Network API",
+        deployedLink: "https://watch.screencastify.com/v/tZbTfOlcRNlVmJXwpcUy",
+        repoLink: "https://github.com/VictoriaCook/social-network-api",
+        gif: socialNetworkApi,
       }
 ];
 
